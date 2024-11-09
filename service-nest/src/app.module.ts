25de8a5ebb07d8099b250ed45172dfe81216e600
Josh Khrustalev <khrustalevjosh@gmail.com>
2024-11-09 15:22:12 +0300

    HS-8: Config NestJS app

--- service-nest/src/app.module.ts
+++ service-nest/src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [],
   controllers: [AppController],
   providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, 'client/browser'),
+    }),
+  ],
 })
 export class AppModule {}
--- service-nest/src/main.ts
+++ service-nest/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  app.setGlobalPrefix('api');
+  app.enableCors({ credentials: true, origin: true });
+  await app.listen(process.env.PORT ?? 8080, '0.0.0.0');
 }
 bootstrap();
