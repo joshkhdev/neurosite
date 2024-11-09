bf831ab3b6822c2b045d8eec1eb78ea05eb40404
Josh Khrustalev <49145950+joshkhdev@users.noreply.github.com>
2024-11-09 13:22:36 +0300

    NS-3: Header and Navigation (#5)
    Closes #3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  standalone: true,
+  imports: [
+    HeaderComponent,
+    RouterOutlet,
+  ],
 })
 export class AppComponent {
-  title = 'neurosite';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,44 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './pages/main/main.component';
+import { NewsComponent } from './pages/news/news.component';
+import { ArticlesComponent } from './pages/articles/articles.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { PsyComponent } from './pages/psy/psy.component';
+import { ShopComponent } from './pages/shop/shop.component';
+import { AboutComponent } from './pages/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'news',
+    component: NewsComponent,
+  },
+  {
+    path: 'articles',
+    component: ArticlesComponent,
+  },
+  {
+    path: 'projects',
+    component: ProjectsComponent,
+  },
+  {
+    path: 'psy',
+    component: PsyComponent,
+  },
+  {
+    path: 'shop',
+    component: ShopComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  }
+];
