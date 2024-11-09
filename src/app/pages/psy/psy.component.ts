a2918d4372eb406967bacec5e62bf8030ac92b26
Josh Khrustalev <khrustalevjosh@gmail.com>
2024-11-09 11:31:57 +0300

    NS-3: Header v1

---
+++ src/app/pages/psy/psy.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-psy',
+  standalone: true,
+  imports: [],
+  templateUrl: './psy.component.html',
+  styleUrl: './psy.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class PsyComponent {
+
+}
---
+++ src/app/shared/components/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+  standalone: true,
+  imports: [
+    RouterLink,
+  ],
+})
+export class HeaderComponent {
+
+}
